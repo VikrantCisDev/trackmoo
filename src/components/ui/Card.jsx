0427765a66efe6d5cd5413cf6d8ede54b8cf711b
rajnish888 <rajnish.p@cisinlabs.com>
2025-03-05 20:38:27 +0530

    Card added in Home

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import Button from './Button';
+
+const CommonCard = ({
+    cardWrapper = 'card-wrapper',
+    imgWrapper = 'img-wrapper',
+    cardImg = '', 
+    alt = 'Card Image',
+    imgClass = 'img-class',
+    imgTag = 'img-tag',
+    children,
+    cardBody = 'card-body',
+    gradeClass = 'grade-class',
+    grade = '',
+    titleClass = 'title-class',
+    title = '',
+    subTitleClass = 'subtitle-class',
+    subTitle = '',
+    featuresClass = 'features-class',
+    features = '',
+    priceClass = 'price-class',
+    price = '',
+    cardBtnClass = 'card-btn-class',
+    cardBtn = '' 
+}) => {
+    return (
+        <div className={cardWrapper}>
+            {cardImg && (
+                <div className={imgWrapper}>
+                    <Image src={cardImg} alt={alt} className={imgClass} />
+                    {children && <span className={imgTag}>{children}</span>}
+                </div>
+            )}
+
+            <div className={cardBody}>
+                {grade && <small className={gradeClass}>{grade}</small>}
+                {title && <h5 className={titleClass}>{title}</h5>}
+                {subTitle && <p className={subTitleClass}>{subTitle}</p>}
+                {features && <p className={featuresClass}>{features}</p>}
+                {price && <h4 className={priceClass}>{price}</h4>}
+                {cardBtn && <Button className={cardBtnClass}>{cardBtn}</Button>} 
+            </div>
+        </div>
+    );
+};
+
+export default CommonCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import CommonCard from '../../components/ui/Card';
+import image1 from '../../assets/images/Card1.jpg'
 
 const Home = () => {
+
+  const cardData = [
+    {
+      id: 1, image: image1, title: 'Cattle', subTitle: 'Bob Mould is celebrating the new year ', features: [
+       
+      ]
+    }
+  ]
+
+
   return (
-    <div className='px-64'>Home </div>
+    <div className='px-64'>
+      <div className="cardOuter">
+        {cardData.map((item) => (
+          <CommonCard
+            key={item.id}
+            cardImg={item.image}
+            title={item.title}
+            subTitle={item.subTitle}
+            features={item.features}
+          />
+        ))}
+
+      </div>
+    </div>
   );
 };
 
