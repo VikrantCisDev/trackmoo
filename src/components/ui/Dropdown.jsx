ec2409db76a4f28fe18376d8712ff1138fb8ec27
vikrantcisdev <vikrant.raj@cisinlabs.com>
2025-03-05 20:00:58 +0530

    dashboard changes

---
+++ src/components/ui/Dropdown.jsx
@@ -0,0 +1,26 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import React from 'react';
+
+function CustomDropdown({ options = [], children }) {
+  return (
+    <Dropdown className='custom-dropdown'>
+      <Dropdown.Toggle variant='success' id='dropdown-basic'>
+        {children}
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {Array.isArray(options) && options.length > 0 ? (
+          options.map((item, key) => (
+            <Dropdown.Item href='#/action-1' key={key}>
+              {item}
+            </Dropdown.Item>
+          ))
+        ) : (
+          <Dropdown.Item disabled>No options available</Dropdown.Item>
+        )}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default CustomDropdown;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import Button from '../components/ui/Button';
 import Search from '../components/ui/Search';
+import { IoMdAdd } from "react-icons/io";
+import CustomDropdown from '../components/ui/Dropdown';
+import { IoFilterOutline } from "react-icons/io5";
 
 const StepOne = () => {
   const [subStep, setSubStep] = useState(1);
@@ -15,14 +18,21 @@ const StepOne = () => {
     }
   };
 
+  const filter = ["action","action2","action3"]
+
   return (
     <div className="main-content dash-SB">
-        csdf
         <div className="dash-container">
           <div className="top-bar">
             <h1>Livestock Marketplace</h1>
             <div className="top-bar-right">
               <Search/>
+              <CustomDropdown options={filter} className="filter">
+                <IoFilterOutline className='me-2' size={23} color='#A6A6A6' />Filter
+              </CustomDropdown>
+              <Button className="fw-normal dash-sellBtn w-100">
+                <IoMdAdd size={22}/> List/ Sell Livestock
+              </Button>
             </div>
           </div>
         </div>
