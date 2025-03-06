51c64a5f9dd917266e1069e43d88ec3785e821bf
vikrantcisdev <vikrant.raj@cisinlabs.com>
2025-03-06 16:04:20 +0530

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+  plugins: [react()],
+});
